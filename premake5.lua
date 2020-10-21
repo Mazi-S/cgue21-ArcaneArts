6b8a21c20798770d274c27d8648f3ed7e64b2b11
Mathias <mathias.schwengerer@gmail.com>
2020-10-21 12:56:29 +0200

    Add GLFW submodule

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,10 @@ workspace "ArcaneArts"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}";
 
 IncludeDir = {}
+IncludeDir["GLFW"] = "%{wks.location}/Engine/dependencies/GLFW/include"
+
+group "Dependencies"
+	include "Engine/dependencies/GLFW"
 
 group ""
 	include "Engine"
